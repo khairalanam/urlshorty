583023a9962f84ab346f1258340ca489bf48a909
khairalanam <anaambasheer@gmail.com>
2023-12-26 23:22:37 +0530

    Configure CORS

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
 
 	// Create a new router and endpoints
 	router := mux.NewRouter()
+	router.Use(CORSMiddleware)
 	router.HandleFunc("/", indexHandler)
 	router.HandleFunc("/{short}", redirectHandler)
 	http.Handle("/", router)
@@ -42,6 +43,21 @@ func main() {
 	http.ListenAndServe("0.0.0.0:" + port, nil)
 }
 
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
